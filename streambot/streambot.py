bd70ff21554590d8f2360c091bbc4906798bfb14
Andrew <andrewmarkoski@gmail.com>
2020-01-17 22:07:03 -0500

    Base bot

--- streambot/streambot.py
+++ streambot/streambot.py
@@ -1,2 +1,21 @@
+import discord
 
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
+tokenFile = open("token.txt","r+")
+token = tokenFile.read()
+client.run(token)
 print("Hello World!")
+tokenFile.close()
