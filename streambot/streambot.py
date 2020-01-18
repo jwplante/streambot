00342481a36add3c69cb9241b0e45b4ba1a20668
JP Bulman <jpbulman@wpi.edu>
2020-01-18 10:52:54 -0500

    Starting basic voting commands WIP

--- streambot/streambot.py
+++ streambot/streambot.py
@@ -1,8 +1,11 @@
 import discord
+from copy import deepcopy
 
 client = discord.Client()
 
 from queue import PriorityQueue
+
+votingTag = 0
 
 q = PriorityQueue()
 
@@ -26,14 +29,25 @@ async def on_message(message):
         args = message.content.split(" ")[1:]
         numberResult = args[0]
         searchPhrase = " ".join(args[1:])
-        await message.channel.send("Adding " + searchPhrase + " to the queue.")
-        q.put((0, searchPhrase))
+        await message.channel.send("Adding " + searchPhrase + " to the queue with voting tag #" + str(votingTag) + ".")
+        q.put((1, searchPhrase, votingTag))
+        # votingTag += 1
     elif message.content.startswith('!showq'):
         finalStr = ""
         temp = list(q.queue)
         for item in temp:
-            finalStr += item[1] + '\n'
+            finalStr += item[1] + ": " + str(item[0]) + " votes with voting tag #" + str(item[2]) + '\n'
         await message.channel.send(finalStr)
+    elif message.content.startswith('!vote'):
+        votedTag = message.content.split(" ")[1]
+        temp = list(q.queue)
+        matchedItem = ""
+        for item in temp:
+            print(item[2], votedTag)
+            if item[2] == int(votedTag):
+                matchedItem = (item[0] + 1, item[1], item[2])
+                q.put(matchedItem)
+                pass
 
 tokenFile = open("token.txt","r+")
 token = tokenFile.read()
